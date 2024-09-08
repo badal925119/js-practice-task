e0cebb3aeb9b5615f71a5590b0f4718d873b4dfb
Badal <badalhasanbd5@gmail.com>
2024-09-08 21:37:21 +0600

    added shopping total cost

--- practice.js
+++ practice.js
@@ -3,17 +3,18 @@
 // Calculate the total cost of the products in a shopping cart
 
 const product =[
-   {name:'Shampo', Price:34},
-   {name:'Shirt', Price:560},
-   {name:'Pant', Price:660},
-   {name:'Shoes', Price:689},
+   {name:'Shampo', Price:34, Quantity:5},
+   {name:'Shirt', Price:560, Quantity:3},
+   {name:'Pant', Price:660, Quantity:6},
+   {name:'Shoes', Price:689, Quantity:4},
 ]
 
 
 function getShoppingTotal(products){
    let total = 0;
    for( const product of products){
-      total = total + product.Price
+      const totalCost =product.Price * product.Quantity
+      total = total + totalCost;
    }
    return total;
 
